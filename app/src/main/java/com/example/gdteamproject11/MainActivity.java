7670ef4e525d85827280cf00cd04171d7f89f090
heondong9265 <heondong0723@gmail.com>
2022-06-01 17:04:24 +0900

    Edit -> mainFragment, toolbar, bottomNavigation

--- app/src/main/java/com/example/gdteamproject11/MainActivity.java
+++ app/src/main/java/com/example/gdteamproject11/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.gdteamproject11;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
