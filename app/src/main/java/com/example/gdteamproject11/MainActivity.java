6f179b254344165a277590059834e8ff3442303c
DESKTOP-QMOOLU8\haksu <haksunlee456@naver.com>
2022-06-03 16:40:34 +0900

    바텀네비 + 프래그먼트 연결

--- app/src/main/java/com/example/gdteamproject11/MainActivity.java
+++ app/src/main/java/com/example/gdteamproject11/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.gdteamproject11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    cardFragment fragmentcard ;
+    mainFragment fragmentmain;
+    mypageFragment fragmentmypage;
+    BottomNavigationView bt_navi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentcard = new cardFragment();
+        fragmentmain = new mainFragment();
+        fragmentmypage = new mypageFragment();
+        bt_navi = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentmain).commit();
+
+        initNavigationBar();
+    }
+    public void initNavigationBar(){
+        bt_navi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentmain).commit();
+                        return true;
+                    case R.id.shopping:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentcard).commit();
+                        return true;
+                    case R.id.my:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragmentmypage).commit();
+                        return true;
+//                    case R.id.location: <----추가예정---->
+//                        getSupportFragmentManager().beginTransaction().commit();
+                }
+                return false;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/gdteamproject11/mainFragment.java
+++ app/src/main/java/com/example/gdteamproject11/mainFragment.java
@@ -17,7 +17,7 @@ public class mainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_main, container,false);
-        setHasOptionsMenu(true);
+
         return viewGroup;
     }
 }
