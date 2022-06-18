ceb399381d10cf1d288439891eb34e5fb57451ca
heondong9265 <heondong0723@gmail.com>
2022-06-18 15:39:09 +0900

    로그인쪽 구현

---
+++ app/src/main/java/com/example/gdteamproject11/User.java
@@ -0,0 +1,9 @@
+package com.example.gdteamproject11;
+
+// 사용자 계정 정보 모델 클래스
+public class UserAccount {
+    public String email;
+    public String name;
+    public String phone;
+    public String ;
+}
