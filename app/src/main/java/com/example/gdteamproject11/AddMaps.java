9316f8ce6ce13787f448fef9e25b257ddaaf8fda
developer Jin <91578450+jininim@users.noreply.github.com>
2022-06-19 17:52:53 +0900

    last

---
+++ app/src/main/java/com/example/gdteamproject11/AddMaps.java
@@ -0,0 +1,80 @@
+package com.example.gdteamproject11;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class AddMaps extends AppCompatActivity implements OnMapReadyCallback {
+    DBHelper dbHelper;
+    SQLiteDatabase sqlDB;
+    private GoogleMap mMap;
+    EditText editMap;
+    Button btnMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_maps);
+        dbHelper = new DBHelper(this,1);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        editMap = findViewById(R.id.edit_map1);
+        btnMap = findViewById(R.id.btn_map1);
+        String str = editMap.getText().toString();
+        sqlDB = dbHelper.getReadableDatabase();
+        Cursor cursor = sqlDB.rawQuery("SELECT * FROM Gdream;", null);
+        String str1;
+
+        while (cursor.moveToNext()) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    LatLng latLng1= new LatLng(Double.parseDouble(cursor.getString(4)),Double.parseDouble(cursor.getString(3)));
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng1);
+                    markerOptions.title(cursor.getString(1));
+                    markerOptions.snippet(cursor.getString(2));
+                    mMap.addMarker(markerOptions);
+                }
+            });
+
+
+        }
+        LatLng latLng = new LatLng(37.381282869618, 126.92872052667);
+        // 성결대 위도 경도 	37.381282869618, 126.92872052667
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+
+        //마커 클릭 이벤트
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener(){
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent it = new Intent(getBaseContext(), AddStore.class);
+
+                startActivity(it);
+            }
+        });
+    }
+
+}
