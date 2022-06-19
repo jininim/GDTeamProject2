2ebe6b42fab6dbef60c83a8b1a979a74fd37c9e7
DESKTOP-QMOOLU8\haksu <haksunlee456@naver.com>
2022-06-19 13:10:38 +0900

    마커 타이틀 스니펫추가

--- app/src/main/java/com/example/gdteamproject11/MapsActivity.java
+++ app/src/main/java/com/example/gdteamproject11/MapsActivity.java
@@ -10,6 +10,8 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,11 +25,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     DBHelper dbHelper;
     SQLiteDatabase sqlDB;
     private GoogleMap mMap;
+    EditText editMap;
+    Button btnMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
         dbHelper = new DBHelper(this,1);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -37,10 +43,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        editMap = findViewById(R.id.edit_map);
+        btnMap = findViewById(R.id.btn_map);
+        String str = editMap.getText().toString();
         sqlDB = dbHelper.getReadableDatabase();
-        Cursor cursor = sqlDB.rawQuery("SELECT * FROM Gdream;", null);
-        while (cursor.moveToNext()) {
 
+        Cursor cursor = sqlDB.rawQuery("SELECT * FROM Gdream;", null);
+
+        while (cursor.moveToNext()) {
 
             runOnUiThread(new Runnable() {
                 @Override
@@ -48,6 +58,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     LatLng latLng1= new LatLng(Double.parseDouble(cursor.getString(4)),Double.parseDouble(cursor.getString(3)));
                     MarkerOptions markerOptions = new MarkerOptions();
                     markerOptions.position(latLng1);
+                    markerOptions.title(cursor.getString(1));
+                    markerOptions.snippet(cursor.getString(2));
                     mMap.addMarker(markerOptions);
                 }
             });
